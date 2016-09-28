94d0a5427d3ce125974aa588d39fbbda5be04ea1
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:22:50 -0700

    addition function works

---
+++ arithmetic.py
@@ -0,0 +1,23 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return
+
+def multiply(num1, num2):
+    return
+
+def divide(num1, num2):
+    return
+
+def square(num1):
+    return
+
+def cube(num1):
+    return
+
+def power(num1, num2):
+    return
+
+def mod(num1, num2):
+    return
